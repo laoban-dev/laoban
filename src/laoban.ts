b6c1224ac0889cb902671ca8b6bc12eddce5369c
phil <phil.rice@validoc.org>
2020-12-01 12:29:46 +0100

    laoban is now just a dsl for commander

--- src/laoban.ts
+++ src/laoban.ts
@@ -14,57 +14,63 @@ export class Cli {
     }
 
     defaultOptions(program: any): any {
-        return program.option('-config, --config <config>', 'Overrides the default config file location')
+        return program.option('-d, --dryrun', 'displays the command instead of executing it', false)
     }
+    projectOptions(program: any): any {
+        return program.option('-d, --dryrun', 'displays the command instead of executing it', false).//
+            option('-a, --all', 'executes this in all projects', false)
+    }
+
     program = require('commander').//
         arguments('').//
-        version('0.1.0').//
-        option('-d, --dryrun', 'displays the command instead of executing it')
+        version('0.1.0')//
+
+
+    addScripts(scripts: ScriptDetails[], options: (program: any) => any) {
+        scripts.forEach(c => {
+            this.command(c.name, c.description, options).action((cmd: any) => {
+                    if (cmd.dryrun) {
+                        let script: ScriptDetails = scripts.find(s => s.name === c.name)
+                        // console.log(scripts)
+                        if (script) {
+                            console.log(script.name + " (" + script.type + ")", script.description)
+                            console.log()
+                            let nameWidth = Math.max(4, Strings.maxLength(script.commands.map(s => s.name)))
+                            let cmdWidth = Math.max(4, Strings.maxLength(script.commands.map(s => s.command)))
+                            console.log('Step'.padEnd(nameWidth), 'Command')
+                            console.log('-'.padEnd(nameWidth, '-'), '-'.padEnd(cmdWidth, '-'))
+                            script.commands.forEach(c => console.log(c.name.padEnd(nameWidth), c.command))
+                        } else
+                            console.log('software error command ' + c.name + " not found. type laoban commands for a list of commands")
+                    } else
+                        console.log('a', cmd.dryrun, c.name, c.description)
+                }
+            )
+        })
+    }
 
     constructor(config: Config) {
-        this.command('commands', 'lists the commands', this.defaultOptions).//
-            action((cmd: any) => {
-                let width = Strings.maxLength(([...config.globalScripts, ...config.projectScripts]).map(s => s.name))
-                if (config.globalScripts) {
-                    console.log('global')
-                    config.globalScripts.forEach(s => console.log('   ', s.name.padEnd(width), s.description))
-                }
-                console.log()
-                if (config.projectScripts) {
-                    console.log('project')
-                    config.projectScripts.forEach(s => console.log('   ', s.name.padEnd(width), s.description))
-                }
-                console.log()
-                console.log("type 'loaban command <name>' for details on the command")
-            });
         this.command('config', 'displays the config', this.defaultOptions).//
             action((cmd: any) => console.log(laoban, JSON.stringify(config, null, 2)))
-
-        this.command('command <command>', 'lists the details on a single command', this.defaultOptions).//
-            action((command: string, cmd: any) => {
-                let scripts = [...config.globalScripts, ...config.projectScripts]
-                let script: ScriptDetails = scripts.find(s => s.name === command)
-                // console.log(scripts)
-                if (script) {
-                    console.log(script.name + " (" + script.type + ")", script.description)
-                    console.log()
-                    let nameWidth = Math.max(4, Strings.maxLength(script.commands.map(s => s.name)))
-                    let cmdWidth = Math.max(4, Strings.maxLength(script.commands.map(s => s.command)))
-                    console.log('Step'.padEnd(nameWidth), 'Command')
-                    console.log('-'.padEnd(nameWidth, '-'), '-'.padEnd(cmdWidth, '-'))
-                    script.commands.forEach(c => console.log(c.name.padEnd(nameWidth), c.command))
-                } else
-                    console.log('command ' + command + " not found. type laoban commands for a list of commands")
-            })
-
+        let scripts = [...config.projectScripts, ...config.globalScripts]
+        this.addScripts(config.projectScripts, this.projectOptions)
+        this.addScripts(config.globalScripts, this.defaultOptions)
 
         var p = this.program
-        this.program.on('command:*', function () {
-            console.error('Invalid command: %s\nSee --help for a list of available commands.', p.args.join(' '));
-            process.exit(1);
-        });
+        this.program
+            .on(
+                'command:*'
+                ,
+                function () {
+                    console.error('Invalid command: %s\nSee --help for a list of available commands.', p.args.join(' '));
+                    process.exit(1);
+                }
+            );
 
-        this.program.allowUnknownOption(false);
+        this.program
+            .allowUnknownOption(
+                false
+            );
 
     }
     parsed: any;
