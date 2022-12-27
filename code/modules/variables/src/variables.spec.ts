153996975573a7cda1c46491d2135406bd098a5d
phil <phil.rice@validoc.org>
2022-12-27 22:54:46 +0100

    Can do mapping of variable (to support 'links' in project.details.json

--- code/modules/variables/src/variables.spec.ts
+++ code/modules/variables/src/variables.spec.ts
@@ -1,4 +1,4 @@
-import { derefence } from "./variables";
+import { derefence, doubleXmlVariableDefn } from "./variables";
 
 describe ( "derefence", () => {
   describe ( "simple variables like ${a}", () => {
@@ -8,7 +8,7 @@ describe ( "derefence", () => {
       expect ( derefence ( 'context', dic, "b.c" ) ).toEqual ( 'b.c' )
       expect ( derefence ( 'context', dic, "Some data ${a} here" ) ).toEqual ( 'Some data A here' )
       expect ( derefence ( 'context', dic, "Some data ${b.c} here" ) ).toEqual ( 'Some data BC here' )
-      expect ( derefence ( 'context', dic, "Some data ${d} here" ) ).toEqual ( "Some data //LAOBAN-UPDATE-ERROR context no value found. ref was ${d}. Value was undefined here" )
+      expect ( derefence ( 'context', dic, "Some data ${d} here" ) ).toEqual ( "Some data //LAOBAN-UPDATE-ERROR context for ref [${d}]. no value found. Value was undefined here" )
     } )
 
   } )
@@ -22,7 +22,7 @@ describe ( "derefence", () => {
 
   } )
 
-  const dic = { a: { 'this': 1, 'item': 2 } }
+  const dic = { a: { 'this': 1, 'item': 2 }, array: [ 'a', 'b', 'c' ] }
   describe ( "variable:object like ${a:object}", () => {
     it ( 'should replace an object with the string w/o {}', () => {
       expect ( derefence ( 'context', dic, '{"one":1,\n${a:object},\n"two":2}' ) ).toEqual (
@@ -34,8 +34,8 @@ describe ( "derefence", () => {
     it ( 'should report an error if the reference isnt an object', () => {
       expect ( derefence ( 'context', dic, '{"one":1,\n${b:object},\n"two":2}' ) ).toEqual (
         '{"one":1,\n' +
-        '//LAOBAN-UPDATE-ERROR context no value found. ref was ${b:object}. Value was undefined,\n' +
-        '"two":2}')
+        '//LAOBAN-UPDATE-ERROR context for ref [${b:object}]. no value found. Value was undefined,\n' +
+        '"two":2}' )
 
     } )
 
@@ -64,6 +64,23 @@ describe ( "derefence", () => {
       expect ( derefence ( 'context', dic, '{"one":1,${a:object:comma:indent3},"two":2}' ) ).toEqual ( `{"one":1,   "this": 1,
    "item": 2,"two":2}` )
     } )
-
   } )
+  describe ( "We should be able to process  {${projectDetails.details.links:map<<>>(i=>\"<<i>>\":\"<<version>>\")}}", () => {
+    it ( 'should inline the mapped array', () => {
+      expect ( derefence ( 'context', { ...dic, version: '1.2.3' },
+        '{"one":1, ${array:map<<>>(i=>"<<i>>":"<<version>>")},"two":2}' ) ).toEqual (
+        '{"one":1, "a":"1.2.3","b":"1.2.3","c":"1.2.3","two":2}' )
+    } )
+  } )
+  describe ( "We should be able to process  {${projectDetails.details.links:comma:map<<>>(i=>\"<<i>>\":\"<<version>>\")}}", () => {
+    it ( "should only add a comma if the array is not empty", () => {
+      expect ( derefence ( 'context', { ...dic, version: '1.2.3' },
+        '{"one":1, ${array:comma:map<<>>(i=>"<<i>>":"<<version>>")}"two":2}' ) ).toEqual (
+        '{"one":1, "a":"1.2.3","b":"1.2.3","c":"1.2.3","two":2}' )
+      expect ( derefence ( 'context', { ...dic, version: '1.2.3' },
+        '{"one":1, ${nothingPresent:comma:map<<>>(i=>"<<i>>":"<<version>>")}"two":2}' ) ).toEqual (
+        '{"one":1, "two":2}' )
+    } )
+  } )
+
 } )
--- code/modules/variables/src/variables.ts
+++ code/modules/variables/src/variables.ts
@@ -10,8 +10,16 @@ export const dollarsBracesVarDefn: VariableDefn = {
   removeStartEnd: ref => ref.slice ( 2, ref.length - 1 )
 }
 export const fulltextVariableDefn: VariableDefn = {
-  regex: /(\$.*^)/g,
+  regex: /(.*^)/g,
   removeStartEnd: ref => ref
+}
+export const mustachesVariableDefn: VariableDefn = {
+  regex: /{{(.*)}}/g,
+  removeStartEnd: ref => ref.slice ( 2, ref.length - 2 )
+}
+export const doubleXmlVariableDefn: VariableDefn = {
+  regex: /<<([^>]*)>>/g,
+  removeStartEnd: ref => ref.slice ( 2, ref.length - 2 )
 }
 interface ProcessedVariableResult {
   result: string
@@ -32,8 +40,11 @@ function variableDefn ( options: DereferenceOptions | undefined ): VariableDefn 
 /** If the string has ${a} in it, then that is replaced by the dic entry */
 export function derefence ( context: string, dic: any, s: string, options?: DereferenceOptions ) {
   const regex = variableDefn ( options ).regex
-  let groups = s.match ( regex )
-  return groups ? groups.reduce ( ( acc, v ) => acc.replace ( v, replaceVar ( context, v, dic, options ) ), s ) : s;
+  let groups: RegExpMatchArray = s.match ( regex )
+  return s.replace ( regex, match => {
+    let result = replaceVar ( context, match, dic, options );
+    return result;
+  } );
 }
 
 
@@ -48,9 +59,8 @@ export function replaceVar ( context: string, ref: string, dic: any, options: De
   const withoutStartEnd = variableDefn ( options ).removeStartEnd ( ref )
   const obj = findVar ( dic, withoutStartEnd )
   const last = lastSegment ( withoutStartEnd, '.' )
-  const { result, error } = processVariable ( last, obj, options )
+  const { result, error } = processVariable ( context, dic, last, obj, options )
   if ( error !== undefined ) {
-    console.log('dic', dic)
     if ( options?.throwError ) {throw new Error ( context + `Ref is ${ref}\n` + safeArray ( error ).join ( ',' ) )} else
       return `//LAOBAN-UPDATE-ERROR ${context} for ref [${ref}]. ${error}. Value was ${JSON.stringify ( obj )}`
   }
@@ -65,12 +75,39 @@ function findIndentString ( parts: string[] ): ProcessedVariableResult {
     return { result: '', error: `Indent had illegal value ${indent}. Needs to be indentx where x is an integer` }
   }
 }
-export function processVariable ( nameWithCommands: string, value: any | undefined, options: DereferenceOptions | undefined ): ProcessedVariableResult {
-  if ( value === undefined && options?.allowUndefined ) return { result: options?.undefinedIs }
+export function processVariable ( context: string, dic: any, nameWithCommands: string, value: any | undefined, options: DereferenceOptions | undefined ): ProcessedVariableResult {
   function error ( error: string | string[] ): ProcessedVariableResult {
     return { result: nameWithCommands, error }
   }
+  let mapIndex = nameWithCommands.indexOf ( ':map<<>>(' );
+  if ( mapIndex >= 0 ) {
+    if ( value === undefined || Array.isArray ( value ) ) {
+      const realvalue = safeArray ( value )
+      let commaIndex = nameWithCommands.indexOf ( 'comma' );
+      const hasCommaRequest = commaIndex > 0 && commaIndex < mapIndex
+      const comma = realvalue.length > 0 && hasCommaRequest ? ',' : ''
+      const map = nameWithCommands.substring ( mapIndex + 8 )
+      const mapParts: RegExpMatchArray = map.match ( /^\(([A-Za-z0-9]*)=>(.*)\)$/ )
+      if ( mapParts === null ) return error ( `The mapFn was not of form '(variable=>strings)' it was ${map}` )
+      const variable = mapParts[ 1 ]
+      const mapFn = mapParts[ 2 ]
+
+      const dirWithVar = { ...dic }
+      const mapped = realvalue.map ( ( s, i ) => {
+        dirWithVar[ variable ] = s
+        let newContext = `${context} processing item ${i} in list [${s}]`;
+        let result = derefence ( newContext, dirWithVar, mapFn, { ...options, variableDefn: doubleXmlVariableDefn } );
+        return result;
+      } )
+      const result = mapped.toString () + comma
+      return { result }
+    } else
+      return error ( `The value is not an array for a map<<>>` )
+  }
+  if ( value === undefined && options?.allowUndefined ) return { result: options?.undefinedIs }
+
   if ( value === undefined ) return error ( 'no value found' )
+
   const parts = nameWithCommands.split ( ':' ).map ( s => s.trim () ).filter ( s => s.length > 0 )
   if ( parts.length === 0 ) return { result: value }
   if ( parts.length === 1 ) return { result: value }
@@ -82,6 +119,7 @@ export function processVariable ( nameWithCommands: string, value: any | undefin
     return { result: value.toString () }
   }
 }
+
 
 function toStringRemovingBraces ( ref: any, indent: string ) {
   const result = JSON.stringify ( ref, null, 2 ).split ( "\n" );
