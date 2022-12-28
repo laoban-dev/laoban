a57991ba0d04a198da6bd1fcf214fc905805aad7
phil <phil.rice@validoc.org>
2022-12-28 16:11:54 +0100

    wip: () files

--- code/modules/laoban/src/update.ts
+++ code/modules/laoban/src/update.ts
@@ -16,14 +16,32 @@ export function copyTemplateDirectoryByConfig ( config: ConfigWithDebug, templat
 interface TemplateControlFile {
   files: CopyFileDetails[]
 }
+export const includeFiles = ( fileOps: FileOps ): TransformTextFn => async ( type: string, text: string ): Promise<string> => {
+  let regExp = /\${include\(([^)]*)\)/g;
+  const includes = text.match ( regExp )
+  if ( !includes ) return Promise.resolve ( text )
+  const urlsAndContent: string [][] = await Promise.all<string[]> ( includes.map ( (includeStr) => {
+    let url = includeStr.slice ( 10, -1 );
+    console.log(includeStr, url)
+    return fileOps.loadFileOrUrl ( url ).then ( text => [ includeStr, text ] );} ) )
+  return text.replace ( regExp, url => urlsAndContent.find ( (urlsAndContent => urlsAndContent[ 0 ] === url) )[ 1 ] )
+};
 
-export const transformFile = ( context: string, dic: any ) => ( type: string, text: string ): Promise<string> => {
+export const transformFile = ( context: string, dic: any ): TransformTextFn => ( type: string, text: string ): Promise<string> => {
   function variableDefn (): VariableDefn {
     if ( type === '${}' ) return dollarsBracesVarDefn
     throw new Error ( `${context}. Unexpected type ${type}` )
   }
   return Promise.resolve ( derefence ( context, dic, text, { throwError: true, variableDefn: variableDefn () } ) )
 };
+
+type TransformTextFn = ( type: string, text: string ) => Promise<string>
+export function combineTransformFns ( ...fns: TransformTextFn[] ): TransformTextFn {
+  return ( type, text ) => fns.reduce <Promise<string>> ( async ( acc, fn ) => fn ( type, await acc ), Promise.resolve ( text ) )
+}
+
+export const includeAndTransformFile = ( context: string, dic: any, fileOps: FileOps ) =>
+  combineTransformFns ( includeFiles ( fileOps ), transformFile ( context, dic ) )
 
 export async function copyTemplateDirectoryFromConfigFile ( fileOps: FileOps, laobanDirectory: string, templateUrl: string, p: ProjectDetailsAndDirectory ): Promise<void> {
   const prefix = templateUrl.includes ( '://' ) ? templateUrl : path.join ( laobanDirectory, templateUrl )
@@ -39,7 +57,8 @@ export async function copyTemplateDirectoryFromConfigFile ( fileOps: FileOps, la
   }
   const controlFileAsString = await fileOps.loadFileOrUrl ( url )
   const controlFile = parseCopyFile ( controlFileAsString );
-  return copyFiles ( `Copying x template ${templateUrl} to ${target}`, fileOps, prefix, target, transformFile ( `Transforming file ${templateUrl} for ${p.directory}`, p ) ) ( safeArray ( controlFile.files ) )
+  return copyFiles ( `Copying x template ${templateUrl} to ${target}`, fileOps, prefix, target,
+    includeAndTransformFile ( `Transforming file ${templateUrl} for ${p.directory}`, p, fileOps ) ) ( safeArray ( controlFile.files ) )
 }
 export function copyTemplateDirectory ( fileOps: FileOps, config: ConfigWithDebug, p: ProjectDetailsDirectoryAndVersion ): Promise<void> {
   let d = config.debug ( 'update' )
