3501c4160960e4c56825568f129cc10eeb0c0107
phil <phil.rice@validoc.org>
2022-12-29 15:14:58 +0100

    wip: improving init

--- code/modules/laoban/src/init.ts
+++ code/modules/laoban/src/init.ts
@@ -1,21 +1,24 @@
-import { Action, ConfigAndIssues } from "./config";
-import * as fs from "fs";
+import { ConfigAndIssues } from "./config";
 import path from "path";
 import { output } from "./utils";
 import { FileOps } from "@phil-rice/utils";
 
-export function init ( fileOps: FileOps, configAndIssues: ConfigAndIssues, dir: string, cmd: any ): Promise<void> {
+export async function init ( fileOps: FileOps, configAndIssues: ConfigAndIssues, dir: string, cmd: any ): Promise<void> {
   const force = cmd.force
   const types = cmd.types
   const listTypes = cmd.listtypes
+  const inits = JSON.parse ( await fileOps.loadFileOrUrl ( configAndIssues.config.inits ) )
   if ( listTypes ) {
-    let inits = configAndIssues.config.inits;
-    configAndIssues.outputStream.write ( `Listing types from  ${inits} ` );
-    fileOps.loadFileOrUrl ( inits ).then ( result => configAndIssues.outputStream.write ( result ) )
+    configAndIssues.outputStream.write ( `Listing types from  ${(configAndIssues.config.inits)}\n${JSON.stringify ( inits, null, 2 )}` );
     return Promise.resolve ()
   }
-  console.log ( 'init', cmd )
-  console.log ( 'init', dir, force, types, listTypes )
+  const errors = types.filter ( t => Object.keys ( inits ).indexOf ( t ) === -1 )
+  if ( errors.length > 0 ) {
+    console.error ( `The following types are not defined : ${errors.join ( ', ' )}. Legal values are ${JSON.stringify ( Object.keys ( inits ) )}` )
+    return Promise.resolve ()
+  }
+  const actualInits = Promise.all ( Object.values(inits).map ( fileOps.loadFileOrUrl ) )
+  console.log ( 'init', actualInits )
   let file = path.join ( dir, 'laoban.json' );
   if ( !force && configAndIssues.config ) return Promise.resolve ( output ( configAndIssues ) ( `This project already has a laoban.json in ${configAndIssues.config.laobanDirectory}. Use --force if you need to create one here` ) )
   return fileOps.saveFile ( file, defaultLaobanJson )
