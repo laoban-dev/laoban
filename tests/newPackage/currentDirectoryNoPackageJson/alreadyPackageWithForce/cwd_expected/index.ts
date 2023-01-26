a2280150e732315b91112d9922996085a7eedc84
phil <phil.rice@validoc.org>
2023-01-26 16:32:42 +0100

    wip: moving towards better package.json merging

--- tests/newPackage/currentDirectoryNoPackageJson/alreadyPackageWithForce/cwd_expected/index.ts
+++ tests/newPackage/currentDirectoryNoPackageJson/alreadyPackageWithForce/cwd_expected/index.ts
@@ -1,3 +1,5 @@
-//Your code goes here
+//Often this will  be just a series of exports like
+//export * from './src/..'
 
+//In the meantime this is a placeholder for the real code
 const replaceme = 'replace me';
